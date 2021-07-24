00068760c6adc26a0db8eac00ffc8d25d44c6c71
Felipe Ornelis <felipedeornelis@gmail.com>
2021-07-24 16:15:59 -0300

    Insert title tag into the document

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -45,6 +45,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
             rel="stylesheet"
           />
+          <title>Tokyo 2020</title>
         </Head>
         <body>
           <Main />
